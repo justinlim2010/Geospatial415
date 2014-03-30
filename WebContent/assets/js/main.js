19a5296db39271f1d0ebfb830d6f17b65f7a0559
alfredkk <akkustedjo.2011@sis.smu.edu.sg>
2014-03-30 18:53:42 +0800

    Modularized main.js and Added ERPGantryPoint.geojson

--- WebContent/assets/js/main.js
+++ WebContent/assets/js/main.js
@@ -1,79 +1,63 @@
-//global variables
-//Defining Web service from Cloudmade and specifying API key
-var qqq;
+/**
+ * Javacript source for Need For Speed Application
+ */
+
+/* Global variables */
 var baseMapLayerURL = 'http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png';
 var baseMapLayerAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>';
-var baseMaps;
-var overlayMaps;
-var polygonZoning;
-var choropleth;
-var incidentsMarkers;
-var zoneMarkers;
-var densityMarkers;
-var config;
-var info = L.control();
-var sidebar;
-// begin script when window loads
-window.onload = initialize(); // ->
+var overlay = new Array();
+var baseMaps, overlayMaps;
+var controlLayer, sidebar;
+var map;
 
-var map; // map object
+/* Run initialize function when HTML document is loaded */
+window.onload = initialize();
 
-// the first function called once the html is loaded
+/* This function will call required function to display the map and all the layers */
 function initialize()
 {
-  setMap();
-  drawPlot();
+  // Set base map
+  setBaseMap();
+  
+  // Set map sidebar
+  setSidebar();
+  
+  // Get real-time data feed from LTA API
+  getRoadIncidentData();
+  
+  // Get road section data and add to overlayMaps
+  var roadSectionFileLocation = "data/RoadSectionLine.geojson";
+  var roadSectionStyle = function( feature ) {
+	  return {
+		  color : "#808080",
+		  weight : 1,
+		  opacity : 1
+		  };
+  };
+  var RoadSectionFeature = function( feature, layer ) {};
+  var roadSectionLayerName = "Road Section";
+  setOverlayMap(roadSectionFileLocation, roadSectionStyle, RoadSectionFeature, roadSectionLayerName);
+  
+  // Get ERP Gantry data and add to overlayMaps
+  var ERPGantryFileLocation = "data/ERPGantryPoint.geojson";
+  var ERPGrantryStyle = function( feature ) {
+	  return {
+		  color : "#0000FF",
+		  weight : 5,
+		  opacity : 1
+		  };
+  };
+  var ERPGantryFeature = function( feature, layer ) {
+	  layer.on( 'mouseover', function( e ){
+		  e.target.bindPopup( feature.properties.description ).openPopup();
+	  } );
+  };
+  var ERPGantryLayerName = "ERP Gantry";
+  setOverlayMap(ERPGantryFileLocation, ERPGrantryStyle, ERPGantryFeature, ERPGantryLayerName);
 };
 
-function style( feature )
-{
-  return {
-    fillColor : getColor( feature.properties.Pop2010 ),
-    weight : 2,
-    opacity : 1,
-    color : 'white',
-    dashArray : '3',
-    fillOpacity : 0.5
-  };
-}
-
-function getColor( d )
-{
-  return d > 8000 ? '#800026' : d > 5000 ? '#BD0026' : d > 1000 ? '#E31A1C' : d > 400 ? '#FC4E2A' : d > 300 ? '#FD8D3C' : d > 200 ? '#FEB24C' : d > 100 ? '#FED976' : '#FFEDA0';
-}
-
-function highlightFeature( e )
-{
-
-  var layer = e.target;
-  info.update( layer.feature.properties.Pop2010 );
-
-  layer.setStyle( {
-    weight : 5,
-    color : '#666',
-    dashArray : '',
-    fillOpacity : 0.7
-  } );
-
-  if ( !L.Browser.ie && !L.Browser.opera )
-  {
-    layer.bringToFront();
-  }
-}
-
-function resetHighlight( e )
-{
-  choropleth.resetStyle( e.target );
-  info.update();
-}
-
-function zoomToFeature( e )
-{
-  map.fitBounds( e.target.getBounds() );
-}
-
-// set basemap parameters
-function setMap()
+/* This function will set the base map */
+function setBaseMap()
 {
   var baseMapLayer = L.tileLayer( baseMapLayerURL, {
     key : '8bc33f0d529540bbbc631f1ae3705abd',
@@ -99,44 +83,65 @@ function setMap()
     zoom : 11,
     layers : [ baseMapLayer ]
   } );
-
+  
+  // Display loading notification
   map.spin( true );
-
-  var geoJsonLayer;
-  var densityLayer;
-  var markerList = [];
-
-  // Custom Control for Info panel
-
-  info.onAdd = function( map )
-  {
-    this._div = L.DomUtil.create( 'div', 'info' ); // create a div with a
-    // class "info"
-    this.update();
-    return this._div;
+  
+  //Layergroup control for basemap
+  baseMaps = {
+    "Basemap" : baseMapLayer,
+    "Chronomap" : chrono,
+    "Night View" : midnight
   };
+  
+  //Layergroup control for basemap
+  overlayMaps = {};
+  
+  controlLayer = L.control.layers( baseMaps, overlayMaps );
+  controlLayer.addTo( map );
+};
 
-  // method that we will use to update the control based on feature properties
-  // passed
-  info.update = function( props )
-  {
-    this._div.innerHTML = '<h4>Traffic Congestion</h4>' + ( props ? '<b>' + props + '</b><br />' + props + ' people ' : 'Hover over a state' );
-  };
-  info.addTo( map );
+/* This function will set the base map */
+function setOverlayMap(fileLoc, layerStyle, layerFeature, layerName) {
+	// Display loading notification
+	map.spin( true );
+	
+	var overlay = L.geoJson( null, {
+	  style : layerStyle,
+	  onEachFeature : layerFeature
+	} );
+	
+	// Add GeoJSON data
+	$.getJSON( fileLoc, function( data )
+	{
+		overlay.addData( data );
+		overlay.addTo( map );
+		console.log( data );
+		
+		// Add layer to layer control
+		overlayMaps[layerName] = overlay;
+		
+		// Remove and update control layer
+		controlLayer.removeFrom( map );
+		controlLayer = L.control.layers( baseMaps, overlayMaps );
+		controlLayer.addTo( map );
+	} );
+}
 
-  $.ajax( {
+function getRoadIncidentData()
+{
+$.ajax( {
     url : "/LTA/TrafficIncidents",
     type : 'GET',
     dataType : 'json'
-  } ).done( function( jsonArray )
+  } )
+  .done( function( jsonArray )
   {
     var features = [];
     $.each( jsonArray, function( index, element )
     {
       $.each( element, function( index, element )
       {
-        qqq = element;
-
         // Create coordinate data
         var coordinate = [ element.long, element.lat ];
 
@@ -173,7 +178,7 @@ function setMap()
 
     console.log( geoJson );
 
-    // Map geoJson
+    // Create layer
     var incidentLayer = L.geoJson( geoJson, {
       onEachFeature : function( feature, layer )
       {
@@ -184,163 +189,34 @@ function setMap()
       }
     } );
 
-    // Marker Cluster
+    // Set markercluster
     var incidentLayerCluster = new L.MarkerClusterGroup();
     incidentLayerCluster.addLayer( incidentLayer );
     map.addLayer( incidentLayerCluster );
-
-    // Added road section
-    var roadSectionLine = L.geoJson( null, {
-      style : function( feature )
-      {
-        return {
-          color : "#000",
-          weight : 2,
-          opacity : 1
-        };
-      }
-    } );
-    // Add road shapefile
-    $.getJSON( "data/RoadSectionLine.geojson", function( data )
-    {
-      roadSectionLine.addData( data );
-      roadSectionLine.addTo( map );
-      console.log( data );
-    } );
-
-    // Layergroup control for basemap
-    baseMaps = {
-      "Basemap" : baseMapLayer,
-      "Chronomap" : chrono,
-      "Night View" : midnight
-    };
-
-    // Layergroup control for additional layer (incidents points)
-    overlayMaps = {
-      "Incidents" : incidentLayerCluster,
-      "Road Section" : roadSectionLine
-    // "Population Density": densityLayer
-    };
-
-    L.control.layers( baseMaps, overlayMaps ).addTo( map );
     
-    sidebar = L.control.sidebar("sidebar", {
-  	  closeButton: true,
-  	  position: "left"
-  	}).addTo(map);
-
-    map.spin( false );
-  } ).fail( function( data )
+    // Add layer to layer control
+	overlayMaps["Incident"] = incidentLayerCluster;
+    
+    // Remove and update control layer
+	controlLayer.removeFrom( map );
+	controlLayer = L.control.layers( baseMaps, overlayMaps );
+	controlLayer.addTo( map );
+  } )
+  .fail( function( data )
   {
     console.log( data );
-    map.spin( false );
   } );
-
-}; // End of setup()
-
-function createLegend()
-{
-  var legend = L.control( {
-    position : 'bottomright'
-  } );
-
-  legend.onAdd = function( map )
-  {
-
-    var div = L.DomUtil.create( 'div', 'info legend' ), incidents = [ 0, 100, 200, 300, 400, 1000, 5000, 8000 ], labels = [];
-
-    // loop through our density intervals and generate a label with a
-    // colored square for each interval
-    for ( var i = 0; i < incidents.length; i++ )
-    {
-      div.innerHTML += '<i style="background:' + getColor( incidents[i] + 1 ) + '"></i> ' + incidents[i] + ( incidents[i + 1] ? '&ndash;' + incidents[i + 1] + '<br>' : '+' );
-    }
-    return div;
-  };
-
-  legend.addTo( map );
 }
 
-function choropleth()
+function setSidebar()
 {
-  var choroplethLayer;
-  $.getJSON( "data/population_density.geojson", function( data )
-  {
-    polygonZoning = L.geoJson( data, {
-      style : function( feature )
-      {
-        return {
-          color : '#888888',
-        };
-      },
-      onEachFeature : function( feature, layer )
-      {
-        layer.on( 'mouseover', function( e )
-        {
-          e.target.bindPopup( feature.properties.name ).openPopup();
-        } );
-      }
-    } ).addTo( map );
-
-  } );
-
-  map.on( 'mouseover', function( e )
-  {
-    console.log( e )
-  } );
-  console.log( polygonZoning );
-  var temp = overlayMaps;
-  overlayMaps = {
-    "Incidents" : temp.Incidents,
-    "Zone" : polygonZoning
-  }
-  console.log( overlayMaps );
+	sidebar = L.control.sidebar("sidebar", {
+	  	  closeButton: true,
+	  	  position: "left"
+	  	}).addTo(map);
 }
 
-function infoPanel()
-{
-  // Custom Control for Info panel
-  var info = L.control();
-  info.onAdd = function( map )
-  {
-    this._div = L.DomUtil.create( 'div', 'info' ); // create a div with a
-    // class "info"
-    this.update();
-    return this._div;
-  };
-
-  // method that we will use to update the control based on feature properties
-  // passed
-  info.update = function( props )
-  {
-    this._div.innerHTML = '<h4>SAN FRANSICO Crime</h4>' + ( props ? '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>' : 'Hover over a state' );
-  };
-
-  info.addTo( map );
-  // End of Custom Control
-}
-
-// R Codes goes here
-
-function drawPlot()
-{
-  // set page to communicate to with "mypackage" on server below
-  ocpu.seturl( "http://localhost:9075/ocpu/library/stats/R" );
-
-  var mydata = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ];
-
-  // call R function: stats::sd(x=data)
-  var req = ocpu.rpc( "sd", {
-    x : mydata
-  }, function( output )
-  {
-    alert( "Standard Deviation equals: " + output );
-  } );
-
-  // optional
-  req.fail( function()
-  {
-    alert( "R returned an error: " + req.responseText );
-  } );
-
-}
+$( document ).ajaxComplete(function() {
+	// Hide loading notification
+	map.spin( false );
+});
